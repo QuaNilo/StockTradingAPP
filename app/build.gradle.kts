26dcc20bfd0a532770286e0be2d2758cd088a86a
Exill18 <franciscorebello2004@gmail.com>
2023-11-12 12:27:02 +0000

    Fixed Duplicated class error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
     // for adding recyclerview
     implementation ("androidx.recyclerview:recyclerview:1.3.2")

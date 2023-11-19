b661b4bb2785c262139b344ba45dd8fa633f2355
DESKTOP/QuaNilo <anyonept911@gmail.com>
2023-11-19 23:18:44 +0000

    Added crude unit tests

---
+++ app/src/test/java/com/example/TPSIMobileProjecto/BackendTest.kt
@@ -0,0 +1,70 @@
+package com.example.TPSIMobileProjecto
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import retrofit.News
+import retrofit.RetrofitHelper
+import retrofit.TickerDetails
+import retrofit.TickerSummary
+import retrofit.retrofitInterface
+import retrofit2.Response
+
+
+class BackendDataTest {
+    private val tickersAPI = RetrofitHelper.getInstance().create(retrofitInterface::class.java)
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private val backend = BackendData()
+
+    @Test
+    fun testGetSymbols() = runBlocking {
+        // Call the function under test
+        val tickersListDeferred =  tickersAPI.getSymbols()
+
+        // Assert that the result is a Deferred<List<String>>
+        assertTrue(tickersListDeferred.isSuccessful)
+        assertTrue(tickersListDeferred is  Response<List<String>>)
+        return@runBlocking
+    }
+
+    @Test
+    fun testGetNews() = runBlocking {
+        // Call the function under test
+        val response =  tickersAPI.getNews()
+
+        // Assert that the result is a Deferred<List<String>>
+        assertTrue(response.isSuccessful)
+        assertTrue(response.body() is  List<News>)
+        return@runBlocking
+    }
+
+    @Test
+    fun testGetTickerDetails() = runBlocking {
+        // Call the function under test
+        val tickersList = backend.getTickersList()
+        val response = tickersList.map { tickersAPI.getSymbolDetails(it) }
+
+        response.map { assertTrue(it.isSuccessful) }
+        response.map { assertTrue(it.body() is TickerDetails) }
+        return@runBlocking
+    }
+
+    @Test
+    fun testGetTickerSummary() = runBlocking {
+        // Call the function under test
+        val tickersList = backend.getTickersList()
+        val response = tickersList.map { tickersAPI.getSymbolSummary(it) }
+
+        response.map { assertTrue(it.isSuccessful) }
+        response.map { assertTrue(it.body() is TickerSummary) }
+        return@runBlocking
+    }
+
+
+
+
+}
+
+
